d6d32d9b79d74e63445c01145d55e51756ff1565
marwoch <marwoch@users.noreply.github.com>
2017-07-20 15:02:08 -0700

    Add project files.

---
+++ EntiyFrameworkDatabaseFirst/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace EntiyFrameworkDatabaseFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboVendors = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboVendors
+            // 
+            this.cboVendors.FormattingEnabled = true;
+            this.cboVendors.Location = new System.Drawing.Point(25, 31);
+            this.cboVendors.Name = "cboVendors";
+            this.cboVendors.Size = new System.Drawing.Size(213, 21);
+            this.cboVendors.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.cboVendors);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboVendors;
+    }
+}
+
---
+++ EntiyFrameworkDatabaseFirst/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntiyFrameworkDatabaseFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateVendorComboBox();
+        }
+
+        private void PopulateVendorComboBox()
+        {
+            //create instance of DBContext
+            APEntities db = new APEntities();
+
+            //object notation query, easy to read
+            List<Vendor> vendors =
+                db
+                .Vendors
+                .Where(v => v.VendorState == "CA")
+                .OrderBy(v => v.VendorName)
+                .ToList();
+
+            ////query syntax
+            //List<Vendor> vendors =
+            //    (from v in db.Vendors
+            //     where v.VendorState == "CA"
+            //     orderby v.VendorName ascending
+            //     select v).ToList();
+
+
+            foreach (Vendor v in vendors)
+            {
+                cboVendors.Items.Add(v.VendorName);
+
+                //foreach (Invoice inv in v.Invoices)
+                //{
+                //    cboVendors.Items.Add(inv.InvoiceNumber);
+                //}
+            }
+        }
+    }
+}
